6870a02729a4b41d7cc0d84aaacb8ebdf4171f7a
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-05-07 21:16:10 +0430

    generate transaction function is update

--- tracker/management/commands/generate_transaction.py
+++ tracker/management/commands/generate_transaction.py
@@ -1,5 +1,6 @@
 import random
-from faker  import Faker 
+from faker import Faker
+
 from django.core.management.base import BaseCommand
 from tracker.models import Transaction,Category, User
 
@@ -10,4 +11,18 @@ class Command(BaseCommand):
         fake = Faker()
         # create category
         categories = ['Bills', 'Foot', 'Cloths', 'Medical', 'Housing','Salary','Social','Transport','Vacations']
-        return 
+        for category in categories:
+            Category.objects.get_or_create(name= category)
+        user = User.objects.filter(username='admin').first()
+        if not user:
+            user = User.objects.create_superuser(username='admins', password='open allah.')
+        categories = Category.objects.all()
+        types =   [x[0] for x in Transaction.TRANSACTION_TYPE_CHOICE]
+        for i in range(20):
+            Transaction.objects.create(
+                category = random.choice(categories),
+                user=user,
+                amount = random.uniform(1,2500),
+                date = fake.date_between(start_date='-1y',end_date='today'),
+                type = types
+            )
