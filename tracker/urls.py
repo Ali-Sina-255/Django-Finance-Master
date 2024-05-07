5e5e41204a5a9399d02c478b8aa08d0ed3fbb061
Ali-Sina-255 <alisinasultani255@gmail.com>
2024-05-07 23:16:54 +0430

    transction list is added

---
+++ tracker/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns  = [
+    path('', views.index, name='index'),
+    path('transactions/', views.transaction_list, name='transactions'),
+]
--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from . models import Transaction
 
-# Create your views here.
+def index(request):
+    return render(request, 'tracker/index.html')
+
+@login_required
+def transaction_list(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    context = {"transactions": transactions}
+    return render(request, 'tracker/transactions-list.html', context)
